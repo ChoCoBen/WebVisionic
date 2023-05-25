0f7f08346f8e4a6be4defc0130fc430d6b48dde5
ChoCoBen <corentin.hubert@imt-atlantique.net>
2023-05-25 17:27:54 -0400

    Setup everything

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResearchComponent } from './page/research/research.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './page/home/home.component';
+import { PublicationsComponent } from './page/publications/publications.component';
+import { TeamComponent } from './page/team/team.component';
+import { ContactComponent } from './page/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResearchComponent,
+    HeaderComponent,
+    HomeComponent,
+    PublicationsComponent,
+    TeamComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
